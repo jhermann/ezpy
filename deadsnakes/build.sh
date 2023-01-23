daf68ab028863bdd56aab5f75a7b34af3b7c6565
jhermann <jh@web.de>
2023-01-24 00:41:22 +0100

    add download links for Python 3.9 .. 3.11

--- deadsnakes/build.sh
+++ deadsnakes/build.sh
@@ -16,6 +16,9 @@ declare -A zip_links
 zip_links["python3.6"]="https://github.com/deadsnakes/python3.6/archive/ubuntu/xenial.zip"
 zip_links["python3.7"]="https://github.com/deadsnakes/python3.7/archive/ubuntu/xenial.zip"
 zip_links["python3.8"]="https://github.com/deadsnakes/python3.8/archive/ubuntu/bionic.zip"
+zip_links["python3.9"]="https://github.com/deadsnakes/python3.9/archive/refs/tags/debian/3.9.16-1+focal1.zip"
+zip_links["python3.10"]="https://github.com/deadsnakes/python3.10/archive/refs/tags/debian/3.10.9-1+focal1.zip"
+zip_links["python3.11"]="https://github.com/deadsnakes/python3.11/archive/refs/tags/debian/3.11.1-1+focal1.zip"
 
 sed -e "s/##ID##/${tag}/g" <Dockerfile.build >Dockerfile.$tag
 
